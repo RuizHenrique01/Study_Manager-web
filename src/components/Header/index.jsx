43863755d220d8cfa8554054711ad56df6bb064d
RuizHenrique01 <affonsohenriqueruiz@gmail.com>
2021-11-23 13:57:55 -0400

    Refatorando componente menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
 import userImage from '~/assets/user.svg';
-import Menu from './Menu';
 import './index.css'
 
 const Header = () => {
@@ -12,13 +11,20 @@ const Header = () => {
         history.push('/perfil');
     }
 
+    function handleProjects() {
+        history.push('/home');
+    }
+
     return (
         <header className="home-header">
+
+            <span className="home-header-option" onClick={handleProjects}>Projects</span>
+
+            <span className="home-header-option">Logout</span>
+
             <button className="home-header-button" type='button' onClick={handlePerfil}>
                 <img className="home-header-image" src={userImage} alt="Imagem do Usuário" />
             </button>
-
-            <Menu />
         </header>
     );
 };
